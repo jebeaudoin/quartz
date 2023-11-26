fcfcf6b411c71b7389e6d6d646fae9afbe544b0f
jebeaudoin <jebeaudoin@users.noreply.github.com>
2023-11-25 22:54:03 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz-680.pages.dev",
+    baseUrl: "",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
